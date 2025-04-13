533079c802321fcb6aba0fc79be1cb5d9607e15b
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2025-04-13 02:46:17 -0400

    enable alteration of embedded SVG via screen inputs

---
+++ assets/js/day2/script-day2.js
@@ -0,0 +1,34 @@
+onload = () => {
+    const randomChangeButton = document.getElementById('chgbtn')
+    if (!randomChangeButton) {
+        throw 'no chgbtn id on page'
+    }
+    randomChangeButton.addEventListener('click',()=>{
+        const rects = document.querySelectorAll('svg rect')
+        if (rects) {
+            const rectCount = rects.length
+            if (rectCount) {
+                const randomIdx = Math.floor(Math.random()*rectCount)
+                const randomColor = getRandomColor()
+                rects[randomIdx].setAttribute('fill',randomColor)
+            }
+        }
+    })
+    const colorButton = document.getElementById('setcolorbtn')
+    if (colorButton) {
+        colorButton.addEventListener('click',()=>{
+            const rects = document.querySelectorAll('svg rect')
+            const idx = parseInt(document.getElementById('txtrow').value)*15 +
+                parseInt(document.getElementById('txtcol').value)
+            rects[idx].setAttribute('fill',document.getElementById('txtcolor').value)    
+        })
+    }
+}
+
+function getRandomColor() {
+    let color = '#'
+    for (let i=0;i<3;i++) {
+        color += Math.floor(Math.random()*255).toString(16).padStart(2,'0')
+    }
+    return color
+}
