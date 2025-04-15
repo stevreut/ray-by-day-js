01ec4c7befb576c148b269a269cabc511ee43207
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2025-04-14 20:38:58 -0400

    Day 6 (BROKEN) - revised bivargrapher.js with anti-aliasing

---
+++ assets/js/day6/bivargrapher.js
@@ -0,0 +1,102 @@
+import GridGraph from "../day4/gridgraph.js";
+
+class BiVariantGrapher {
+    constructor(width,height,pixelSize,pixelsPerUnit = 1,bvf,antiAlias = 1) {
+        if (typeof pixelsPerUnit !== 'number' ||
+            Number.isNaN(pixelsPerUnit) ||
+            pixelsPerUnit <= 0) {
+                throw 'invalid pixelsPerUnit'
+            }
+        this.pixelsPerUnit = pixelsPerUnit
+        if (!bvf) {
+            this.colorFunction = null
+        } else {
+            if (typeof bvf !== 'function') {
+                throw 'bvf argument is not a function'
+            } else {
+                this.colorFunction = bvf
+            }
+        }
+        if (typeof antiAlias !== 'number' || 
+            Number.isNaN(antiAlias) ||
+            antiAlias !== Math.floor(antiAlias) ||
+            antiAlias < 1 || antiAlias > 5) {
+                throw 'invalid antiAlias argument'
+            }       
+        this.grid = new GridGraph(width,height,pixelSize,'#ffffff' /*TODO*/)
+        this.width = width
+        this.height = height
+        this.antiAlias = antiAlias
+        this.usingAntiAlias = (this.antiAlias !== 1)
+        if (this.usingAntiAlias) {
+            this.antiAliasDelta = 1/(this.pixelsPerUnit*this.antiAlias)
+            this.antiAliasOffset = (1-this.antiAlias)/2*this.antiAliasDelta
+            this.antiAliasMultiplier = this.antiAlias**(-2)
+        }
+    }
+    setFunction(bvf) {
+        if (typeof bvf !== 'function') {
+            throw 'bvf argument is not a function'
+        } else {
+            this.colorFunction = bvf
+        }
+    }
+    drawGraph() {
+        if (!this.colorFunction) {
+            throw 'function not defined'
+        }
+        for (let j=0;j<this.height;j++) {
+            let y = (this.height/2-j)/this.pixelsPerUnit  // TODO
+            for (let i=0;i<this.width;i++) {
+                let x = (i-this.width/2)/this.pixelsPerUnit  // TODO
+                let realColorArray = null
+                if (!this.usingAntiAlias) {
+                    realColorArray = this.colorFunction(x,y)
+                } else {
+                    let realColorArray = [0,0,0]
+                    for (let ii=0;ii<this.antiAlias;ii++) {
+                        let xx = x+ii*this.antiAliasDelta+this.antiAliasOffset
+                        for (let jj=0;jj<this.antiAlias;jj++) {
+                            let yy = y+jj*this.antiAliasDelta+this.antiAliasOffset
+                            let addendColor = this.colorFunction(x,y)
+                            for (let k=0;k<3;k++) {
+                                realColorArray[k] += addendColor[k]
+                            }
+                        }
+                    }
+                    realColorArray = realColorArray.map(itm=>itm*this.antiAliasMultiplier)
+                }
+                let pixelColor = null
+                if (this.#validColorArray(realColorArray)) {
+                    pixelColor = this.#bracketRealColor(realColorArray)
+                } else {
+                    pixelColor = [0,0,0]
+                }
+                this.grid.putPixel(i,j,...pixelColor)
+            }
+        }
+        return this.grid.getHTMLElement()
+    }
+    #validColorArray(arr) {
+        if (!Array.isArray(arr) || arr.length !== 3) {
+            return false
+        }
+        let isValid = true
+        arr.forEach(num=>{
+            if (typeof num !== 'number' || Number.isNaN(num)) {
+                isValid = false
+            }
+        })
+        return isValid
+    }
+    #bracketRealColor(rColr) {
+        let pixCol = rColr.map(prim=>{
+            let realPrim = prim*255
+            realPrim = Math.min(255,Math.max(0,realPrim))
+            return Math.round(realPrim)
+        })
+        return pixCol
+    }
+}
+
+export default BiVariantGrapher
---
+++ assets/js/day6/script-day6.js
@@ -0,0 +1,57 @@
+
+import BiVariantGrapher from "./bivargrapher.js"
+
+const svgAnchorID = "svghere"
+
+const PIXELS_WIDTH = 160
+const PIXELS_HEIGHT = 120
+const PIXEL_SIZE = 5  // One VIRTUAL pixel = 5 screen pixels
+
+onload = () => {
+    const svgAnchor = document.getElementById(svgAnchorID)
+    if (!svgAnchor) {
+        throw 'no ' + svgAnchorID + 'id found on page'
+    }
+    svgAnchor.innerHTML = ''
+    let svgElem = createSvgElemAt(svgAnchor)
+    svgAnchor.appendChild(svgElem)
+}
+
+function createSvgElemAt(anch) {
+    let graph = new BiVariantGrapher(200,150,4,60.2,null,3)
+    graph.setFunction(localFunc2)
+    let svgElem = graph.drawGraph()
+    return svgElem
+}
+
+function localFunc1(x,y) {
+    let insideCircle = (x*x+y*y<=1)
+    if (insideCircle) {
+        return [(x+1)/2,0.5,(y+1)/2]
+    } else {
+        return [(5-x)/8,(5-y)/10,0.2]
+    }
+}
+
+let lightVector = [5,3,1]
+
+function localFunc2(x,y) {
+    const r2 = x*x+y*y
+    const insideCircle = (r2<=1)
+    if (!insideCircle) {
+        return [0,0,0]
+    }
+    const z = Math.sqrt(1-r2)
+    const globeVector = [x,y,z]
+    let sum = 0
+    for (let i=0;i<3;i++) {
+        sum += globeVector[i]*lightVector[i]
+    }
+    if (sum <= 0) {
+        return [0.2,0.2,0.2] 
+    } else {
+        let m = sum*0.1+0.2
+        return [m,m*1.1,m*1.2]
+    }
+}
+
