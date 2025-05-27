bb91190c4ccad3a608d7143fff959c32cf08ff22
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2025-05-27 07:05:21 -0400

    adjust sun color in SunnySky and reorganize constants

--- assets/js/day25/sunny-sky.js
+++ assets/js/day25/sunny-sky.js
@@ -2,11 +2,13 @@ import Vector3D from "../day20/vector3d.js"
 import Sky from "../day22/sky.js"
 
 class SunnySky extends Sky {
-    SUN_YELLOW_BASE = [1,1,0.6875] //[1,0.9,0.5]
+    SUN_YELLOW_BASE = [1,0.98046875,0.6875]
     SUN_MULTIPLIER = 3
     SUN_YELLOW = this.SUN_YELLOW_BASE.map(prim=>prim*this.SUN_MULTIPLIER)
     SUN_COS = 0.999989
     SUN_AURA_COS = 0.7
+    AURA_MULTIPLIER = 0.2
+    SUN_AURA_COS_COMPLEMENT = 1-this.SUN_AURA_COS
     constructor(sunDirection) {
         super()
         console.log('SunnySky constructor invoked')
@@ -28,10 +30,10 @@ class SunnySky extends Sky {
         if (cosSunAngle >= this.SUN_COS) {  // TODO
             result = result.map((prim,idx)=>prim+this.SUN_YELLOW[idx])
         } else if (cosSunAngle >= this.SUN_AURA_COS) {
-            let k = (cosSunAngle-this.SUN_AURA_COS)/(1-this.SUN_AURA_COS)
-            k = k*k
-            k *= 0.2 // TODO
-            result = result.map((prim,idx)=>prim+this.SUN_YELLOW[idx]*k)
+            let a = (cosSunAngle-this.SUN_AURA_COS)/this.SUN_AURA_COS_COMPLEMENT
+            a = a*a
+            a *= this.AURA_MULTIPLIER
+            result = result.map((prim,idx)=>prim+this.SUN_YELLOW[idx]*a)
         }
         return result
     }
