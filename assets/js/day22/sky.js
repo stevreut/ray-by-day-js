28d9c22b42558a27970456f0d575211dc929cef8
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2025-06-01 21:10:01 -0400

    refactor relationship between classes Sky and SunnySky; tweak constants in SunnySky for better effect

--- assets/js/day22/sky.js
+++ assets/js/day22/sky.js
@@ -7,16 +7,19 @@ class Sky extends OpticalObject {
     interceptDistance() {
         return Number.POSITIVE_INFINITY
     }
-    handle(ray) {
-        const dir = ray.getDirection().normalized()
-        const inColor = ray.color
-        const z = dir.getZ()
+    colorOfDirection(dir) {
+        const z = dir.normalized().getZ()
         const zMod = z**0.15
         const z2 = 1-zMod
-        let newColor = []
+        let skyColor = []
         for (let i=0;i<3;i++) {
-            newColor.push((this.SKY_BLUE[i]*zMod+this.HORIZ_DUSK[i]*z2)*inColor[i])
+            skyColor.push(this.SKY_BLUE[i]*zMod+this.HORIZ_DUSK[i]*z2)
         }
+        return skyColor
+    }
+    handle(ray) {
+        const skyColor = this.colorOfDirection(ray.getDirection())
+        const newColor = ray.color.map((prim,idx)=>prim*skyColor[idx])
         return newColor
     }
 }
--- assets/js/day25/sunny-sky.js
+++ assets/js/day25/sunny-sky.js
@@ -3,11 +3,12 @@ import Sky from "../day22/sky.js"
 
 class SunnySky extends Sky {
     SUN_YELLOW_BASE = [1,0.98046875,0.6875]
-    SUN_MULTIPLIER = 3
+    SUN_MULTIPLIER = 5
     SUN_YELLOW = this.SUN_YELLOW_BASE.map(prim=>prim*this.SUN_MULTIPLIER)
-    SUN_COS = 0.999989
+    SUN_COS = 0.999 // TODO - restore to 0.999989 (?)
     SUN_AURA_COS = 0.7
-    AURA_MULTIPLIER = 0.2
+    AURA_MULTIPLIER = 0.6
+    AURA_YELLOW = this.SUN_YELLOW_BASE.map(prim=>prim*this.AURA_MULTIPLIER)
     SUN_AURA_COS_COMPLEMENT = 1-this.SUN_AURA_COS
     constructor(sunDirection) {
         super()
@@ -23,16 +24,17 @@ class SunnySky extends Sky {
         this.sunDir = sunDirection.normalized()
     }
     handle (ray) {
-        let result = super.handle(ray)
-        let cosSunAngle = ray.getDirection().cosAngleBetween(this.sunDir)
-        if (cosSunAngle >= this.SUN_COS) {  // TODO
-            result = result.map((prim,idx)=>prim+this.SUN_YELLOW[idx])
+        const dir = ray.getDirection()
+        let cosSunAngle = dir.cosAngleBetween(this.sunDir)
+        let skyColor = super.colorOfDirection(dir)
+        if (cosSunAngle >= this.SUN_COS) {
+            skyColor = skyColor.map((prim,idx)=>prim+this.SUN_YELLOW[idx])
         } else if (cosSunAngle >= this.SUN_AURA_COS) {
             let a = (cosSunAngle-this.SUN_AURA_COS)/this.SUN_AURA_COS_COMPLEMENT
             a = a*a
-            a *= this.AURA_MULTIPLIER
-            result = result.map((prim,idx)=>prim+this.SUN_YELLOW[idx]*a)
+            skyColor = skyColor.map((prim,idx)=>prim+this.AURA_YELLOW[idx]*a)
         }
+        const result = ray.color.map((prim,idx)=>prim*skyColor[idx])
         return result
     }
 }
